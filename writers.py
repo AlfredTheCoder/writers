830d9959c3de9276ff0a4111e043f2ca25b00779
AlfredTheCoder <engineeralfredmuriithi@gmail.com>
2016-10-28 18:44:15 +0300

    last commit

--- writers.py
+++ writers.py
@@ -1,3 +1,8 @@
 primeNos = []
 for no in range(90):
-  prime = True
+    prime = True
+for i in range(n):
+    if ((no % i) == 0):
+        prime = False
+if (prime = True):
+    primeNos.append(2,no)
